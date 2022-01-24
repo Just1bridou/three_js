1f9584fc5021cb8023a8b571a392792d85153338
Justin A-M <justin@omnibook.com>
2022-01-24 11:24:37 +0100

    rework cam

--- script.js
+++ script.js
@@ -3,124 +3,95 @@ import { OrbitControls } from "https://unpkg.com/three@0.120.0/examples/jsm/cont
 var card_front = "img/card_front.png"
 var card_back = "img/card_back.png"
 
-
-var scene,
-  camera,
-  renderer,
-  controls,
-  frontcard,
-  backcard;
-
-var options = {
-  isanimate: false,
-};
-
-// var gui = new dat.GUI();
-// var isanim = gui.addFolder("Animate");
-// isanim.add(options, "isanimate").name("Animate");
-// isanim.open();
-
-// gui.close()
+var camera, controls, scene, renderer;
+var frustumSize = 1000;
+init();
+render();
+animate();
 
 function init() {
-  var container = document.getElementById("world");
-
-  camera = new THREE.PerspectiveCamera(
-    30,
-    1301 / 2 / window.innerHeight,
-    1,
-    10000
-  );
-  camera.position.z = 100;
-
   scene = new THREE.Scene();
+
   renderer = new THREE.WebGLRenderer({ antialias: true, autoSize: true, alpha: true });
- 
-  renderer.setPixelRatio(2);
-  renderer.setSize(1301 / 2, window.innerHeight);
 
-  controls = new OrbitControls(camera, renderer.domElement);
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
 
-  /**
-   * Inertia
-   */
-  controls.enableDamping = true;
-  controls.dampingFactor = 0.03;
+  var container = document.getElementById( 'world' );
+  container.appendChild( renderer.domElement );
 
-  /**
-   * Zoom
-   */
+  var aspect = window.innerWidth / window.innerHeight;
+  camera = new THREE.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 1, 1000 );
+        
+  camera.position.z = 500;
+
+  controls = new OrbitControls( camera, renderer.domElement );
+  controls.addEventListener( 'change', render );
+
+  // controls.enableDamping = true;
+  // controls.dampingFactor = 0.25;
+
   controls.enableZoom = true;
-  controls.maxDistance = 150
-  controls.minDistance = 70
+  controls.maxZoom = 3
+  controls.minZoom = 2
 
-  controls.update();
-  document.getElementById("world").appendChild(renderer.domElement);
-  
-  cardFront();
-  cardBack();
+  // world
 
-  animate();
+  var geometry = new THREE.PlaneGeometry( 200, 300 );
+  //var material =  new THREE.MeshPhongMaterial( { color:0xffffff, shading: THREE.FlatShading } );
+
+  // for ( var i = 0; i < 500; i ++ ) {
+
+    // let x = ( Math.random() - 0.5 ) * 1000;
+    // let y = ( Math.random() - 0.5 ) * 1000;
+    // let z = ( Math.random() - 0.5 ) * 1000;
+
+    let x = 0;
+    let y = 0;
+    let z = 0;
+    
+    mapImage(card_front, (material) => {
+
+      var mesh = new THREE.Mesh(geometry, material);
+
+      mesh.position.x = x;
+      mesh.position.y = y;
+      mesh.position.z = z;
+      mesh.updateMatrix();
+      mesh.matrixAutoUpdate = false;
+      scene.add( mesh );
+    })
+
+    mapImage(card_back, (material) => {
+
+      var mesh = new THREE.Mesh(geometry, material);
+      mesh.rotation.set(0, Math.PI, 0);
+      //scene.add(mesh);
+      mesh.position.x = x;
+      mesh.position.y = y;
+      mesh.position.z = z;
+      mesh.updateMatrix();
+      mesh.matrixAutoUpdate = false;
+      scene.add( mesh );
+    })
+  // }
+
+  window.addEventListener( 'resize', onWindowResize, false );
+
 }
 
-function cardFront() {
-  var geometry = new THREE.PlaneGeometry(20, 30);
-
-  mapImage(card_front, (material) => {
-    frontcard = new THREE.Mesh(geometry, material);
-    scene.add(frontcard);
-  })
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
-function cardBack() {
-  var geometry = new THREE.PlaneGeometry(20, 30);
-
-  mapImage(card_back, (material) => {
-    backcard = new THREE.Mesh(geometry, material);
-    backcard.rotation.set(0, Math.PI, 0);
-    scene.add(backcard);
-  })
+function animate() {
+  requestAnimationFrame( animate );
+  controls.update(); // required if controls.enableDamping = true, or if controls.autoRotate = true
+  render();
 }
 
-function mapImage(url, cb) {
-  var img = new Image()
-  var canvas = document.createElement("canvas"),
-  ctx = canvas.getContext("2d")
-
-  img.crossOrigin = "Anonymous";
-
-  img.onload = function() {
-    canvas.width = img.width;
-    canvas.height = img.height;
-    ctx.drawImage( img, 0, 0 );
-
-    var mat=new THREE.MeshBasicMaterial();
-    mat.map = new THREE.CanvasTexture(canvas);
-
-    cb(mat)
-  }
-  img.src = url;
+function render() {
+  renderer.render( scene, camera );
 }
-var modelgroup = new THREE.Group();
-
-var matrix = new THREE.Matrix4();
-var period = 5;
-var clock = new THREE.Clock();
-
-function updateDraw(deltaTime) {
-  modelgroup.rotation.set(-camera.rotation._x, -camera.rotation._y, 0);
-  if (options.isanimate) {
-    matrix.makeRotationY((clock.getDelta() * 0.7 * Math.PI) / period);
-    camera.position.applyMatrix4(matrix);
-    camera.lookAt(frontcard.position);
-  }
-}
-
-function animate(deltaTime) {
-  requestAnimationFrame(animate);
-  updateDraw(deltaTime);
-  controls.update();
-  renderer.render(scene, camera);
-}
-
-window.addEventListener("load", init, false);
